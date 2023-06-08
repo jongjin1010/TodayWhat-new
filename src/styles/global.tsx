eefec85e7057d7c4d5d81d52381731b6df4d3b66
jongjin1010 <jongjin024@gmail.com>
2023-06-08 14:44:09 +0900

    🎉 :: Begin!

---
+++ src/styles/global.tsx
@@ -0,0 +1,34 @@
+import { Global, css } from "@emotion/react";
+import emotionReset from "emotion-reset";
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${emotionReset}
+
+      body {
+        font-family: "Noto Sans KR", sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      a {
+        text-decoration: none;
+        color: black;
+      }
+      button {
+        background: inherit;
+        border: none;
+        box-shadow: none;
+        border-radius: 0;
+        padding: 0;
+        overflow: visible;
+        cursor: pointer;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
